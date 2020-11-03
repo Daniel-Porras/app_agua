93911826fe89a7e5d36262892f1cfd37e50e795e
Daniel-Porras <dporrasa@unal.edu.co>
2020-11-03 18:17:47 -0500

    Ingreso de la informacion inicial(parte visual)

--- app/src/main/java/com/example/aplicacion_agua/MainActivity2.java
+++ app/src/main/java/com/example/aplicacion_agua/MainActivity2.java
@@ -3,12 +3,18 @@ package com.example.aplicacion_agua;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private Spinner sp1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        sp1 = (Spinner) findViewById(R.id.spinner);
+        String[] opciones = {"1", "2", "3", "4","5","6"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.spinner_item_daniel, opciones);
+        sp1.setAdapter(adapter);
     }
 }
