b2478625661e6b6b30400e713aa7e72242d40171
Daniel-Porras <dporrasa@unal.edu.co>
2020-11-06 18:29:22 -0500

    Pruebas del commit :(

--- app/src/main/java/com/example/aplicacion_agua/MainActivity3.java
+++ app/src/main/java/com/example/aplicacion_agua/MainActivity3.java
@@ -10,12 +10,5 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-        int h;
-        h=hola();
     }
-    public int hola(){
-        int a=5,b=4,c;
-        c=a+b;
-        return c;
-    }
-}//Y
+}
