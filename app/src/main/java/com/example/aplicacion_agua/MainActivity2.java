054dd3ac997baf7bcbf2cb3dc6d6281d6f00f8da
Daniel-Porras <dporrasa@unal.edu.co>
2020-11-13 20:01:29 -0500

    Parte lógica ingreso de usuario

--- app/src/main/java/com/example/aplicacion_agua/MainActivity2.java
+++ app/src/main/java/com/example/aplicacion_agua/MainActivity2.java
@@ -6,24 +6,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
     private Spinner sp1;
+    private EditText usuario;
+    private EditText ultConsumo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        // opciones del estrato social
+        // llamar los elementos de la parte gráfica
+        usuario= (EditText) findViewById(R.id.nombreUsuario);
+        ultConsumo=(EditText)findViewById(R.id.consumoUltimo);
         sp1 = (Spinner) findViewById(R.id.spinner);
+        // opciones del estrato social
         String[] opciones = {"1", "2", "3", "4","5","6"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.spinner_item_daniel, opciones);
         sp1.setAdapter(adapter);
     }
     //Cambiar a la actividad 3 (Boton empezar)
-    public void siguiente(View view){
-        Intent intent =new Intent(MainActivity2.this, MainActivity3.class);
-        startActivity(intent);
-        finish();
+    public void resgistro(View view){
+        String nombre =usuario.getText().toString();
+        String consumo1 =ultConsumo.getText().toString();
+        if (nombre.length()==0){
+            Toast.makeText(this, "Debes ingresar un nombre de usuario",Toast.LENGTH_LONG).show();
+        }
+        if (consumo1.length()==0){
+            Toast.makeText(this, "Debes ingresar el último consumo",Toast.LENGTH_LONG).show();
+        }
+        if (nombre.length()!=0 && consumo1.length()!=0){
+            Intent intent =new Intent(MainActivity2.this, MainActivity3.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
