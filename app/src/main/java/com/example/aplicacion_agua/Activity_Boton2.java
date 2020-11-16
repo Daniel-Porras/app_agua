2c60bcf9283c80714727e5ea8f7723c0863fa8ee
Lchitiva <72529907+Lchitiva@users.noreply.github.com>
2020-11-15 21:19:26 -0500

    Actualización, actividad del botón 1 funcional

---
+++ app/src/main/java/com/example/aplicacion_agua/Activity_Boton2.java
@@ -0,0 +1,14 @@
+package com.example.aplicacion_agua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Boton2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__boton2);
+    }
+}
