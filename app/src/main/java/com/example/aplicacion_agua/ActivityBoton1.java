0f06c5325a67fbcdf439334c87cd2990f1fc5e77
Lchitiva <72529907+Lchitiva@users.noreply.github.com>
2020-11-15 20:25:26 -0500

    Actualización, actividad del botón 1

---
+++ app/src/main/java/com/example/aplicacion_agua/ActivityBoton1.java
@@ -0,0 +1,33 @@
+package com.example.aplicacion_agua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+//Esta parte es la activity del boton 1
+public class ActivityBoton1 extends AppCompatActivity {
+    private EditText cantidad_ahorrar
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boton1);
+        cantidad_ahorrar = findViewById(R.id.cantidad_ahorrar);
+    }
+    //Boton para volver al menú
+    public void Volver(View view){
+        Intent volver = new Intent(this, MainActivity3.class);
+        startActivity(volver);
+    }
+    //Validar si ingresó un número
+    public void validar(View view){
+        String cantidad = cantidad_ahorrar.getText().toString();
+        if (cantidad.length()==0){
+            Toast.makeText(this, "Debes ingresar una cantidad válida",Toast.LENGTH_LONG).show();
+    }
+
+}
--- app/src/main/java/com/example/aplicacion_agua/MainActivity3.java
+++ app/src/main/java/com/example/aplicacion_agua/MainActivity3.java
@@ -2,7 +2,9 @@ package com.example.aplicacion_agua;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+    }
+    public void boton1(View view){
+        Intent boton1 = new Intent(this, ActivityBoton1.class);
+        startActivity(boton1);
     }
 }
