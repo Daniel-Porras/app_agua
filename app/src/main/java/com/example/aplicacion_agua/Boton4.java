17666d2e94cdd770bee8a23e5aa770311b3a6def
Daniel-Porras <dporrasa@unal.edu.co>
2020-11-19 16:51:52 -0500

    Guardar datos del usuario (nombre , estrato, ultconsumo)1.2

--- app/src/main/java/com/example/aplicacion_agua/Boton4.java
+++ app/src/main/java/com/example/aplicacion_agua/Boton4.java
@@ -2,7 +2,9 @@ package com.example.aplicacion_agua;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Boton4 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Boton4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boton4);
+    }
+    public void Volver(View view){
+        Intent volver = new Intent(this, MainActivity3.class);
+        startActivity(volver);
     }
 }
